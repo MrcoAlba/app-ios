c4dec6a4197fb9762aae19569e66994338874eb1
MrcoAlba <mrcoalba@gmail.com>
2024-07-14 12:58:36 -0500

    Initial Commit

---
+++ app-ios/app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_iosApp.swift
+//  app-ios
+//
+//  Created by Mrco on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
